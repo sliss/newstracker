23560c7597f512d3eeaebc3517efe83d51489262
sliss <stevenliss45@gmail.com>
2014-02-03 18:10:08 -0500

    reads keys and site list from text files, searches multiple sites

--- extractLinks.py
+++ extractLinks.py
@@ -4,55 +4,48 @@ from bs4 import BeautifulSoup
 
 
 # keys to search
-keys = list()
 
-keys.append('albert'.lower())
-keys.append('IMAGE'.lower())
-keys.append('sign'.lower())
+keys=list()
+raw_keys = [line.strip() for line in open('keys.txt')]
+
+# ensure keys are all lowercase
+for key in raw_keys:
+	keys.append(key.lower())
 
 for key in keys:
 	print key;
 
+# list of sites to search
+url_list = [line.strip() for line in open('sites.txt')]
 
-# get page's html
-page = urllib2.urlopen('http://www.google.com').read()
-soup = BeautifulSoup(page)
-
-raw_links = list()
+# links that match a key
 matching_links = list()
 
-# add all a tags, and convert contents to lowercase
-for link in soup.find_all('a'):
-	raw_links.append(link)
+# for each site...
+for url in url_list:
 
-# search each raw link for key, and add matches to matching_links
-for link in raw_links:
-    for key in keys:
-    	if key in str(link).lower():
-    		matching_links.append(link)
+	# get page's html
+	page = urllib2.urlopen(url).read()
+	soup = BeautifulSoup(page)
 
-for link in raw_links:
-	print link;
+	raw_links = list()
 
+	# add all <a> tags, and convert contents to lowercase
+	for link in soup.find_all('a'):
+		raw_links.append(link)
+
+	# search each raw link for key, and add matches to matching_links
+	for link in raw_links:
+		for key in keys:
+			if key in str(link).lower():
+				matching_links.append(link)
+
+	for link in raw_links:
+		print link;
+
+# output results
 print("RESULTS")
 
 for link in matching_links:
 	s = link.get_text() + ': ' + str(link.get('href'));
 	print s
-#soup.find_all(a=re.compile("elsie"))
-
-
-    
-'''
-def extract_urls(page):
-    url_re = re.compile(r'\b(([\w-]+://?|www[.])[^\s()<>]+(?:\([\w\d]+\)|([^[:punct:]\s]|/)))')
-    for match in url_re.finditer(page):
-        yield match.group(0)
-
-for uri in extract_urls(page):
-    urls.append(uri)
-    
-
-
-'''
-
